5ba19b8b26db3944b819578d181c9a263fb89aa0
Kamil Adamczyk <kamada37@gmail.com>
2019-04-14 14:47:17 +0200

    Options final adjustments

--- deckers-back/models/option.js
+++ deckers-back/models/option.js
@@ -4,7 +4,7 @@ var optionSchema = new mongoose.Schema({
     name: String,
     short: String,
 
-    values: []
+    values: [Number]
 });
 
 module.exports = mongoose.model("Option", optionSchema);
