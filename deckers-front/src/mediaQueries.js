68bc6c0375215b93a1f6e29f9f66409be79033e9
Kamil Adamczyk <kamada37@gmail.com>
2019-11-08 20:38:00 +0100

    Media queries added

---
+++ deckers-front/src/mediaQueries.js
@@ -0,0 +1,22 @@
+const size = {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktopS: '1650px',
+    desktop: '1900px'
+}
+
+export const device = {
+    mobileS: `(min-width: ${size.mobileS})`,
+    mobileM: `(min-width: ${size.mobileM})`,
+    mobileL: `(min-width: ${size.mobileL})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+    laptopL: `(min-width: ${size.laptopL})`,
+    desktopS: `(min-width: ${size.desktopS})`,
+    desktop: `(min-width: ${size.desktop})`,
+    desktopL: `(min-width: ${size.desktop})`
+};
